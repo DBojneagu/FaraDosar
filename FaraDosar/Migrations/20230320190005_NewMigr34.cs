8620fa3d3bf9e297df1e422c005483df07400a8d
Bojneagu David <david.bojneagu@yahoo.com>
2023-03-20 21:26:09 +0200

    ProfilesDone

---
+++ FaraDosar/Migrations/20230320190005_NewMigr34.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FaraDosar.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigr34 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Privacy",
+                table: "Profiles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Privacy",
+                table: "Profiles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
