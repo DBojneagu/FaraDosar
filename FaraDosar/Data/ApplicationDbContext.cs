f019dff961b7b8f42452a60ca4829056ae6b722f
DBojneagu <93039914+DBojneagu@users.noreply.github.com>
2023-03-20 14:11:31 +0200

    UsersDone

--- FaraDosar/Data/ApplicationDbContext.cs
+++ FaraDosar/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FaraDosar.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FaraDosar.Data
@@ -8,6 +9,7 @@ namespace FaraDosar.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
+    }
+        public DbSet<Card> Cards { get; set; }
     }
 }
