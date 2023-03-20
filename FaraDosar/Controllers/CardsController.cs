71abe9aa7f52feab29143e83f11bac5dc2325045
DBojneagu <93039914+DBojneagu@users.noreply.github.com>
2023-03-20 14:33:58 +0200

    UserAgain

---
+++ FaraDosar/Controllers/CardsController.cs
@@ -0,0 +1,43 @@
+﻿using FaraDosar.Data;
+using FaraDosar.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaraDosar.Controllers
+{
+    [Authorize]
+    public class CardsController : Controller
+    {
+
+        private readonly ApplicationDbContext db;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public CardsController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            db = context;
+
+            _userManager = userManager;
+
+            _roleManager = roleManager;
+        }
+
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Index()
+        {
+            var cards = db.Cards.Include("User");
+            ViewBag.Cards = cards;
+
+
+            return View();
+        }
+    }
+}
---
+++ FaraDosar/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FaraDosar.Models
+{
+    public class Card
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Titlul este obligatoriu")]
+        [StringLength(100, ErrorMessage = "Titlul nu poate avea mai mult de 100 de caractere")]
+        [MinLength(5, ErrorMessage = "Titlul trebuie sa aiba mai mult de 5 caractere")]
+        public string Title { get; set; }
+
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
