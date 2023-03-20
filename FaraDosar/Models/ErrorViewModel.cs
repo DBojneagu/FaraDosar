cd5c326c7f9e41ef75e6aa982389a45246c8a6c8
DBojneagu <93039914+DBojneagu@users.noreply.github.com>
2023-03-20 12:40:27 +0200

    First Commit

---
+++ FaraDosar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FaraDosar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
